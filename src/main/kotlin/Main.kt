cc0b2d4ae74e8303b980d886f43fb0c63c82e2ac
Roman <rudenk0-r0man@yandex.com>
2022-07-03 17:40:01 +0300

    All done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
-fun main(args: Array<String>) {
+fun main() {
     val datas = mapOf (
         "Jan" to listOf(100,100,100,100),
         "Feb" to listOf(200,200,-190,200),
@@ -13,6 +13,7 @@ fun printInfo (data: Map<String, List<Int>>){
     val monthlyProfit = MutableList<Int>(0){0}
     var temp = 0
     var mounth = ""
+    var resultWrongMonths = "Month with corrupted data: "
     //for (i in 0 until 4)
     //    println(data.filter { it.value.all { it >= 0 } }.map { it.value }[i])
     for (i in 0 until data.filter { it.value.all { it >= 0 } }.size){
@@ -27,14 +28,26 @@ fun printInfo (data: Map<String, List<Int>>){
             mounth = key
         }
     }
+    for ((key, value) in data.filter { it.value.any { it < 0 }}){
+        resultWrongMonths += "$key, "
+    }
+
     monthlyProfit.sortDescending()
     println("Medium weekly profit: ${data.filter { it.value.all { it >= 0} }.flatMap{ it.value }.average()}")
     println("Medium monthly profit: ${monthlyProfit.average()}")
     println("Max monthly profit: ${monthlyProfit[0]}")
     println("Max monthly profit were in $mounth")
-    println("Min monthly profit: ${1}")
-    println("Min monthly profit were in ${1}")
-    println("Month with corrupted data: ${1}")
+    for ((key, value) in data){
+        if (temp > value.sum()) {
+            temp = value.sum()
+            mounth = key
+        }
+        if (temp == 0)
+            temp = value.sum()
+    }
+    println("Min monthly profit: ${monthlyProfit.last()}")
+    println("Min monthly profit were in $mounth")
+    println(resultWrongMonths.substring(0, resultWrongMonths.length - 2))
 
 
 }
